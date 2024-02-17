31b56a585f09beb0a22c718cbc1a1b9b64229d1b
kvicich <honkhonkovic842@gmail.com>
2024-02-18 01:13:03 +0700

    0.05
    + Добавлена команда /steal
    + Кулдауны перенесены в переменные в начале кода
    + Добавлено парочку сообщений для кражи

--- bot.py
+++ bot.py
@@ -6,7 +6,12 @@ import logging
 import random
 import time
 
+# Переменные
 SERVERS_DATA_DIR = "servers_data"  # Папка с данными серверов
+WORK_COOLDOWN = 1 # Время в секундах между попытками зароботка
+STEAL_COOLDOWN = 5  # Время в секундах между попытками кражи
+FAILED_STEAL_MIN_LOSS = 1 # Минимальная потеря монет
+FAILED_STEAL_MAX_LOSS = 350 # Максимальная потеря монет
 
 logger = logging.getLogger('discord_bot')
 intents = discord.Intents.default()
@@ -70,8 +75,8 @@ async def work_cmd(ctx):
     last_work_time = bot.last_work_time.get(server_id, {})
     if user_id in last_work_time:
         time_elapsed = current_time - last_work_time[user_id]
-        if time_elapsed < 1 * 1:
-            time_left = 1 * 1 - time_elapsed
+        if time_elapsed < WORK_COOLDOWN:
+            time_left = WORK_COOLDOWN - time_elapsed
             await ctx.send(f'{ctx.author.mention}, вы уже работали недавно. Подождите еще {int(time_left)} секунд.')
             return
     last_work_time[user_id] = current_time
@@ -100,6 +105,45 @@ async def balance_cmd(ctx):
     user_data = load_user_data(server_id, user_id)
     user_balance = user_data.get("balance", 0)
     await ctx.send(f'{ctx.author.mention}, ваш текущий баланс: {user_balance} :coin:')
+
+@bot.command(name='steal')
+async def steal_cmd(ctx):
+    user_id = str(ctx.author.id)
+    server_id = str(ctx.guild.id)
+    current_time = time.time()
+    last_steal_time = bot.last_steal_time.get(server_id, {})
+    if user_id in last_steal_time:
+        time_elapsed = current_time - last_steal_time[user_id]
+        if time_elapsed < STEAL_COOLDOWN:
+            time_left = STEAL_COOLDOWN - time_elapsed
+            await ctx.send(f'{ctx.author.mention}, вы недавно уже пытались что-то украсть. Подождите еще {int(time_left)} секунд.')
+            return
+    last_steal_time[user_id] = current_time
+    bot.last_steal_time[server_id] = last_steal_time
+
+    if random.random() < 0.5:  # Шанс 50%
+        stolen_amount = random.randint(20, 265)
+        with open("steal_message.txt", "r", encoding="utf-8") as file:
+            messages = file.readlines()
+            steal_message = random.choice(messages).strip()
+
+        user_data = load_user_data(server_id, user_id)
+        user_balance = user_data.get("balance", 0)
+        user_balance += stolen_amount
+        user_data["balance"] = user_balance
+        save_user_data(server_id, user_id, user_data)
+
+        steal_message = steal_message.replace("{stolen_amount}", str(stolen_amount))
+        await ctx.send(f'{ctx.author.mention}, {steal_message}')
+    else:
+        lost_amount = random.randint(FAILED_STEAL_MIN_LOSS, FAILED_STEAL_MAX_LOSS)
+        user_data = load_user_data(server_id, user_id)
+        user_balance = user_data.get("balance", 0)
+        user_balance = max(0, user_balance - lost_amount)  # Не позволяйте отрицательный баланс
+        user_data["balance"] = user_balance
+        save_user_data(server_id, user_id, user_data)
+
+        await ctx.send(f'{ctx.author.mention}, попытка украсть провалилась! Вы потеряли {lost_amount} монет.')
 
 @bot.event
 async def on_error(event, *args, **kwargs):
@@ -135,6 +179,7 @@ def main():
     server_id = "your_server_id_value"
     file_checker("work_message.txt", server_id)
     bot.last_work_time = {}
+    bot.last_steal_time = {}
     token = get_token()
     if token is not None:
         bot.run(token)
