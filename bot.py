556772ed9a15bb37c978bf4ff35ab4bf8e8c995b
kvicich <honkhonkovic842@gmail.com>
2024-02-17 14:55:39 +0700

    Версия 0.03
    + Добавлена команда /ping (возможно не рабочая)
    + Команда /work вернулась!
    + Для команды /work добавлены рандомные фразы
    + Теперь ошибки записи токена будут выводится в консоль
    + Больше никаких РґРµРЅРµРі ведь теперь у нас кодировка UTF-8
    ~ Теперь логи в консоли повляются сразу после импорта библиотек
    ~ Для удобства некоторые комментарии к коду были перенесены

--- bot.py
+++ bot.py
@@ -1,50 +1,62 @@
 import os
+import json
 import discord
 from discord.ext import commands
 import logging
+import random
+import time
 
 logger = logging.getLogger('discord_bot')
 intents = discord.Intents.default()
 intents.messages = True
+# Выводим логи в консоль
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
-def get_token():
-    token_directory = os.path.dirname(os.path.abspath(__file__))
-    token_file_path = os.path.join(token_directory, "TOKEN.txt")
-
-    if os.path.exists(token_file_path):
-        with open(token_file_path, "r") as file:
-            token = file.read().strip()
-            if token:
-                return token
-            else:
-                print("Токен не найден в файле TOKEN.txt")
-                return None
+# Проверка наличия файла и создание его при необходимости
+def file_checker(file_path, server_id):
+    if not os.path.exists(file_path):
+        with open(file_path, 'w'):
+            pass
+        print(f"Файл {file_path} был создан.")
     else:
-        print("Файл TOKEN.txt не найден")
-        token = input("Введите токен: ")
-        try:
-            with open(token_file_path, "w") as file:
-                file.write(token)
-                print("Токен успешно записан в файл!")
-            return token
-        except Exception as e:
-            print(f"Не удалось записать токен в файл: {e}")
-            return None
+        print(f"Файл {file_path} уже существует.")
+    server_dir = os.path.join(SERVERS_DATA_DIR, str(server_id))
+    if not os.path.exists(server_dir):
+        os.makedirs(server_dir)
+    else:
+        print(f"Папка {server_dir} уже существует.")
 
-token = get_token()
+SERVERS_DATA_DIR = "servers_data"
 
-if token is not None:
-    # Здесь вы можете использовать переменную token для запуска вашего бота
-    pass
-else:
-    print("Не удалось получить токен. Программа будет закрыта.")
-    exit()
+def user_data_path(server_id, user_id):
+    return os.path.join(SERVERS_DATA_DIR, str(server_id), f"{user_id}.json")
+
+def load_user_data(server_id, user_id):
+    data_path = user_data_path(server_id, user_id)
+    if os.path.exists(data_path):
+        with open(data_path, "r") as f:
+            return json.load(f)
+    else:
+        # Если файл данных пользователя отсутствует, создаем новый и возвращаем пустой словарь
+        with open(data_path, "w") as f:
+            json.dump({}, f)
+        return {}
+
+def save_user_data(server_id, user_id, data):
+    data_path = user_data_path(server_id, user_id)
+    with open(data_path, "w") as f:
+        json.dump(data, f)
 
 @bot.event
 async def on_ready():
     print(f"Бот запущен, его имя {bot.user.name}")
+
+# Функция для начисления валюты за работу
+def work_currency():
+    return random.randint(15, 135)
 
 @bot.command(name='start')
 async def start_cmd(ctx):
@@ -52,13 +64,99 @@ async def start_cmd(ctx):
     guild_name = ctx.guild.name
     await ctx.send(f'Приветствую, это на данный момент тестовая команда. Все команды: /help Имя бота: {bot.user.name}. Твой юзернейм: {user_name}. Имя сервера: {guild_name}. Тестовое эмодзи: :fly: ')
 
-def Main():
-    # Выводим логи в консоль
-    logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
+@bot.command(name='work')
+async def work_cmd(ctx):
+    user_id = str(ctx.author.id)
+    server_id = str(ctx.guild.id)
+    current_time = time.time()
+    last_work_time = bot.last_work_time.get(server_id, {})
+    if user_id in last_work_time:
+        time_elapsed = current_time - last_work_time[user_id]
+        if time_elapsed < 60 * 15:  # 60 * 15 = 15 минут, число 15 отвечает за время
+            time_left = 60 * 15 - time_elapsed
+            await ctx.send(f'{ctx.author.mention}, вы уже работали недавно. Подождите еще {int(time_left)} секунд.')
+            return
+    last_work_time[user_id] = current_time
+    bot.last_work_time[server_id] = last_work_time
 
-    # Запускаем бота
-    bot.run(token)
+    currency_earned = work_currency()
+
+    with open("work_message.txt", "r", encoding="utf-8") as file:
+        messages = file.readlines()
+        work_message = random.choice(messages).strip()
+
+    # Загрузка данных пользователя
+    user_data = load_user_data(server_id, user_id)
+    
+    # Получение баланса пользователя с учетом возможного отсутствия ключа 'balance'
+    user_balance = user_data.get("balance", 0)
+    
+    # Увеличение баланса на заработанную валюту
+    user_balance += currency_earned
+
+    # Обновление данных пользователя
+    user_data["balance"] = user_balance
+    
+    # Сохранение данных пользователя
+    save_user_data(server_id, user_id, user_data)
+
+    # Заменяем {currency_earned} на фактическое значение в сообщении
+    work_message = work_message.replace("{currency_earned}", str(currency_earned))
+    
+    await ctx.send(f'{ctx.author.mention}, {work_message}.')
+
+@bot.command(name='balance')
+async def balance_cmd(ctx):
+    user_id = str(ctx.author.id)
+    server_id = str(ctx.guild.id)
+    
+    # Загрузка данных пользователя
+    user_data = load_user_data(server_id, user_id)
+    
+    # Получение баланса пользователя с учетом возможного отсутствия ключа 'balance'
+    user_balance = user_data.get("balance", 0)
+
+    await ctx.send(f'{ctx.author.mention}, ваш текущий баланс: {user_balance} валюты')
+
+@bot.event
+async def on_error(event, *args, **kwargs):
+    logger.error(f"Произошла ошибка в событии {event}", exc_info=True)
+
+def get_token():
+    token_directory = os.path.dirname(os.path.abspath(__file__))
+    token_file_path = os.path.join(token_directory, "TOKEN.txt")
+
+    if os.path.exists(token_file_path):
+        try:
+            with open(token_file_path, "r") as file:
+                token = file.read().strip()
+                if token:
+                    return token
+                else:
+                    logger.error("Токен не найден в файле TOKEN.txt")
+                    return None
+        except Exception as e:
+            logger.error(f"Ошибка при чтении токена из файла: {e}")
+            return None
+    else:
+        logger.error("Файл TOKEN.txt не найден")
+        return None
+    
+@bot.command(name='ping')
+async def ping(ctx):
+    start_time = time.time()
+    end_time = time.time()
+    ping_time = round((end_time - start_time) * 1000)
+    await ctx.send(f"Время пинга: {ping_time} мс")
+
+def main():
+    file_checker("work_message.txt") # Проверяем наличие файлов
+    bot.last_work_time = {} # Словарь для хранения времени последнего использования команды /work для каждого сервера
+    token = get_token()
+    if token is not None:
+        bot.run(token) # Запускаем бота
+    else:
+        logger.error("Не удалось получить токен. Бот выключается...")
 
 if __name__ == '__main__':
-    Main()
+    main()
